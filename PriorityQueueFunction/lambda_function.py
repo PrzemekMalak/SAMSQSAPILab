83cb81f17b57c16f7396e1c50a59fb50f7ead29f
EC2 Default User <ec2-user@ip-172-31-39-186.eu-west-1.compute.internal>
2019-04-25 14:56:10 +0000

    Cleanup

--- PriorityQueueFunction/lambda_function.py
+++ PriorityQueueFunction/lambda_function.py
@@ -1,9 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
     print(json.dumps(event))
-    x = 2 / 0
+    #x = 2 / 0
     return {
         "statusCode": 200,
         "body": json.dumps('Priority')
--- StandardQueueFunction/lambda_function.py
+++ StandardQueueFunction/lambda_function.py
@@ -1,7 +1,6 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
     print(json.dumps(event))
     return {
         "statusCode": 200,
